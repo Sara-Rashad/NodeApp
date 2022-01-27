59203a97a1f0bfb41e0f665e368189ff43d3178f
sara.rashad <sara.rashad2012@gmail.com>
2022-01-28 00:54:09 +0200

    learn modules

--- app.js
+++ app.js
@@ -1,5 +1,61 @@
-function SayHello(name){
-    console.log("hello "+name);
-}
+const LoggerModule = require('./logger');
+const path = require('path');
+const os = require('os');
+const fs = require('fs');
+const http=require('http');
 
-SayHello('sara');
+/******************** Test Http Module  ********************/
+const server=http.createServer((request,response)=>{
+    if(request.url==='/'){
+        response.write('Hello ....');
+        response.end();
+    }
+    if(request.url==='/api/courses'){
+        response.write(JSON.stringify([1,2,3]));
+        response.end();
+    }
+    
+});
+
+server.listen(3000);
+console.log('Listening to port 3000 ...');
+
+/******************** Test Event Module Class ********************/
+const logger=new LoggerModule();
+//Add listenter
+logger.on('messageLogged',function(arg){
+console.log('Listenner Called',arg);
+});
+
+logger.log('My message');
+
+// emitter.on('messageLogging',(args)=>{
+// console.log('Logging.. ',args);
+// });
+
+
+/******************** Test file system module  ********************/
+//sync read directory
+const files = fs.readdirSync('./');
+console.log(files);
+
+//Async read directory (recommended)
+fs.readdir('.ss', function (err, result) {
+    if (err)
+        console.log('Error', err);
+    else
+        console.log('Result', result);
+});
+
+/**********Test os module ********************/
+var freeMem=os.freemem();
+var totalMem=os.totalmem();
+console.log(`Free Memory ${freeMem}`);
+console.log(`Total Memory ${totalMem}`);
+
+/******************** Test path module ********************/
+var pathObj=path.parse(__filename);
+console.log(pathObj);
+
+/******************** Test custom module ********************/
+LoggerModule.log('Custom Module : My message');
